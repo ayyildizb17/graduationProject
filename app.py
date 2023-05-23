61dbe256986f0d113ff17e23d7b6280a2691adc0
incipasa <43948071+incipasa@users.noreply.github.com>
2023-05-23 09:55:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,5 +18,19 @@ def login():
         # Invalid credentials, redirect back to the login page
         return redirect('/')
 
+@app.route('/tsne')
+def tsne():
+    return render_template('tsne.html')
+
+@app.route('/word2vec')
+def word2vec():
+    return render_template('word2vec.html')
+
+@app.route('/kmeans')
+def kmeans():
+    return render_template('kmeans_page.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
